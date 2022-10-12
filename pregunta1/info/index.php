e23ac0a9f733cf37b785499291e0398222f03c9c
GioNayo <115657521+GioNayo@users.noreply.github.com>
2022-10-12 12:43:50 -0500

    Add files via upload

---
+++ pregunta1/info/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+   <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/slick.css" type="text/css" /> 
+    <link rel="stylesheet" href="../css/templatemo-style.css">
+<!--
+    
+TemplateMo 560 Astro Motion
+
+https://templatemo.com/tm-560-astro-motion
+
+-->
+</head>
+<body>
+    <video autoplay muted loop id="bg-video">
+        <source src="../video/gfp-astro-timelapse.mp4" type="video/mp4">
+    </video>
+    <div class="page-container">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="cd-slider-nav">
+              <nav class="navbar navbar-expand-lg" id="tm-nav">
+                <a class="navbar-brand" href="../index.php">FACULTAD DE CIENCIAS <br> PURAS Y NATURALES</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-supported-content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbar-supported-content">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                      <li class="nav-item selected">
+                        <a class="nav-link" aria-current="page" href="#0" data-no="1">INICIO</a>
+                        <div class="circle"></div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#0" data-no="2">CARRERAS</a>
+                        <div class="circle"></div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#0" data-no="3">CONTACTOS</a>
+                        <div class="circle"></div>
+                      </li>
+                    
+                    </ul>
+                  </div>
+              </nav>
+            </div>
+          </div>          
+        </div>        
+      </div>      
+      <div class="container-fluid tm-content-container">
+        <ul class="cd-hero-slider mb-0 py-5">
+          <li class="px-3" data-page-no="1">
+            <div class="page-width-1 page-left">
+              <div class="d-flex position-relative tm-border-top tm-border-bottom intro-container">
+                <div class="intro-left tm-bg-dark">
+                  <h2 class="mb-4">Bienvenido a FCPN</h2>
+                  <p class="mb-4">
+                  La Carrera de Matemática de la UMSA, posee un nivel académico de excelencia, forma profesionales e investigadores, comprometidos con la sociedad y sus necesidades; orientados a la creación de conocimientos mediante los enfoques de la Matemática pura y aplicada, que respondan a los retos científicos y tecnológicos de la sociedad, enfatizando en la formación integral orientada al desarrollo y la creación de conocimientos matemáticos en las áreas de la Matemática Pura y Aplicada, para lograr la transformación del país con responsabilidad social y contribuyendo a su desarrollo sostenible.    
+                </p>
+                  
+                </div>
+                <div class="intro-right">
+                  <img src="../img/info.jpg" alt="Image" class="img-fluid intro-img-1">
+                  
+                </div>
+                <div class="circle intro-circle-1"></div>
+                <div class="circle intro-circle-2"></div>
+                <div class="circle intro-circle-3"></div>
+                <div class="circle intro-circle-4"></div>
+              </div>
+              <div class="text-center">
+                <a href="#0" data-page-no="2" class="btn btn-primary tm-intro-btn tm-page-link">
+                  CARRERAS
+                </a>
+              </div>            
+            </div>            
+          </li>
+          <li data-page-no="2">
+            <!-- Image Carousel -->
+            <div class="mx-auto position-relative gallery-container">
+              <div class="circle intro-circle-1"></div>
+              <div class="circle intro-circle-2"></div>
+              <div class="mx-auto tm-border-top gallery-slider">
+                <figure class="effect-julia item">
+                    <img src="../img/mate.png" alt="Image">
+                    <figcaption>
+                        <div>
+                            <p>CARRRERA DE MATEMATICA</p>
+                        </div>
+                        <a href="../mate/index.php">Ver mas</a>
+                    </figcaption>
+                </figure>
+              <!--
+                  <figure class="effect-julia item">
+                    <img src="../img/info.jpg" alt="Image">
+                    <figcaption>
+                        <div>
+                            <p>CARRERA DE INFORMATICA</p>
+                        </div>
+                        <a href="#">Ver mas</a>
+                    </figcaption>
+                </figure>
+              -->
+                <figure class="effect-julia item">
+                    <img src="../img/biologia.jpg" alt="Image">
+                    <figcaption>
+                        <div>
+                            <p>CARRERA DE BIOLOGIA</p>
+                        </div>
+                        <a href="../biologia/index.php">Ver mas</a>
+                    </figcaption>
+                </figure>
+                <figure class="effect-julia item">
+                    <img src="../img/estadistica.jpg" alt="Image">
+                    <figcaption>
+                        <div>
+                            <p>CARRERA DE ESTADISTICA</p>
+                        </div>
+                        <a href="../estadistica/index.php">Ver mas</a>
+                    </figcaption>
+                </figure>
+               
+              </div>
+            </div>
+          </li>
+          <li data-page-no="3" class="px-3">
+            <div class="position-relative page-width-1 page-right tm-border-top tm-border-bottom">
+              <div class="circle intro-circle-1"></div>
+              <div class="circle intro-circle-2"></div>
+              <div class="circle intro-circle-3"></div>
+              <div class="circle intro-circle-4"></div>
+              <div class="tm-bg-dark content-pad">
+                <h2 class="mb-4">PARA MAS INFORMACION</h2>
+               
+                <p>
+                  Dirección: Av. Villazón Nº 1995, Plaza del Bicentenario - Zona Central, La Paz - Bolivia.<BR>
+                  Telf.: (591-2) 2441570 / 2440571<BR>
+                  Fax: 2441092<BR>
+                  Email: decanato@fcpn.edu.bo<br>
+                  <a href="https://es-la.facebook.com/fcpn.umsa/">FACEBOOK </a> <BR>
+                  <a href="https://www.youtube.com/channel/UCziXp6vAr6dQMfgz_dy_hNA">YOUTUBE</a>
+                  
+                </p>
+                
+              </div>              
+            </div>
+          </li>
+         
+        </ul>
+    </div>
+    <div class="container-fluid">
+      <footer class="row mx-auto tm-footer">
+        <div class="col-md-6 px-0">
+          Copyright 2021 Astro Motion Company Limited. All rights reserved.
+        </div>
+        <div class="col-md-6 px-0 tm-footer-right">
+          Designed by <a rel="sponsored" href="https://templatemo.com" target="_blank" class="tm-link-white">TemplateMo</a>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Preloader, https://ihatetomatoes.net/create-custom-preloading-screen/ -->
+  
+  <script src="../js/jquery-3.5.1.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/slick.js"></script>
+  <script src="../js/templatemo-script.js"></script>
+</body>
+</html>sssss
